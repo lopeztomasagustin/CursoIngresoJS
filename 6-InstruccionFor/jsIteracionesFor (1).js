5a6052c47a28e05e34b8a04fc9357fad3420e7be
lopeztomasagustin <52410714+lopeztomasagustin@users.noreply.github.com>
2019-08-05 09:24:38 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,9 @@
 function mostrar()
 {
+var i;
+
+for ( i = 1; i < 10; i++){
+   document.write( i + " "); 
+}
 
 }
--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,9 @@
 function mostrar()
 {
+var int;
 
+for(int = 10; int > 0; int--){
+document.write(int + " ");
+}
 
 }
